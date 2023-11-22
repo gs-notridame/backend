d0d16794343d05c985b1cebde7f30c605de4a5e1
matheusPierro <matpierro570@gmail.com>
2023-11-22 15:35:08 -0300

    Feat: create a spring project and first api (Hello World) to do some tests on cloud

---
+++ src/test/java/com/system/harmonia/HarmoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.harmonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
