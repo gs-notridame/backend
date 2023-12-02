b794612aa5c1d2ee82b103a8e176644f74286d06
matheusPierro <matpierro570@gmail.com>
2023-12-01 23:22:23 -0300

    fixed artefacts

--- src/main/java/com/system/psycheck/config/CorsConfiguration.java
+++ src/main/java/com/system/psycheck/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://10.0.0.4:8080")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
